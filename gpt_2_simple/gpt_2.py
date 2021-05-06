d596a227d93ffbb7899befed9e97a6e180f146c3
Howard888 <prioleau1123@gmail.com>
2021-05-06 14:31:32 -0700

    added get_logits to gpt2 simple

--- gpt_2_simple/gpt_2.py
+++ gpt_2_simple/gpt_2.py
@@ -403,6 +403,36 @@ def load_gpt2(sess,
         print('Loading checkpoint', ckpt)
     saver.restore(sess, ckpt)
 
+def get_logits(sess,
+              checkpoint='latest',
+              run_name="run1",
+              checkpoint_dir="checkpoint",
+              model_name=None,
+              model_dir='models',
+              multi_gpu=False):
+    """Loads the model checkpoint or existing model into a TensorFlow session
+    for repeated predictions.
+    """
+
+    if model_name:
+        checkpoint_path = os.path.join(model_dir, model_name)
+    else:
+        checkpoint_path = os.path.join(checkpoint_dir, run_name)
+
+    hparams = model.default_hparams()
+    with open(os.path.join(checkpoint_path, 'hparams.json')) as f:
+        hparams.override_from_dict(json.load(f))
+
+    context = tf.compat.v1.placeholder(tf.int32, [1, None])
+
+    gpus = []
+    if multi_gpu:
+        gpus = get_available_gpus()
+
+    output = model.model(hparams=hparams, X=context, gpus=gpus)
+
+    return output
+
 
 def generate(sess,
              run_name='run1',
